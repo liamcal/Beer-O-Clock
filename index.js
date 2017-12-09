858e299d3fc12844505f9d2b8c1ec8b8583ee630
Harry Park <harryparkdotio@users.noreply.github.com>
2017-12-09 17:08:26 +1100

    more fixes

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const currentTime = () => {
-	return new Date();
+let currentTime = () => {
+	let d = new Date();
+	return new Date(d);
 }
-
-const getCurrentTime = currentTime;
 
 const nextFriday = () => {
 	let date = new Date(currentTime());
@@ -14,14 +13,15 @@ const nextFriday = () => {
 	return date.getTime();
 }
 
-function timeToBeer() {
+const timeToBeer = () => {
 	var perc = 1 - Math.abs(currentTime().getTime() - (nextFriday() - 604800)) / Math.abs(((nextFriday() - 604800) - nextFriday())) / 1000;
 	setTimeout(() => {
 		document.getElementsByClassName('beer')[0].classList.add('filled');
 	}, 300);
-	document.getElementsByClassName('liquid')[0].style.height = perc * 28 + 'vmin';
+
+	document.getElementsByClassName('liquid')[0].style.height = ((perc > 0.075 ? perc : 0.075) * 28) + 'vmin';
 	if ((((nextFriday()) - currentTime().getTime()) / 1000) < 254 && document.getElementById('countdown_song').paused) {
-		document.getElementById('countdown_song').currentTime = 254 - ((nextFriday() - getCurrentTime().getTime()) / 1000);
+		document.getElementById('countdown_song').currentTime = 254 - ((nextFriday() - currentTime().getTime()) / 1000);
 		document.getElementById('countdown_song').play();
 	}
 	if (currentTime().getDay() == 5 && currentTime().getHours() < 17) {
@@ -36,52 +36,45 @@ function timeToBeer() {
 		}
 		document.getElementsByClassName('beer')[0].classList.remove('carbonate');
 	}
-	return (nextFriday() - getCurrentTime().getTime());
+	return (nextFriday() - currentTime().getTime());
 }
 
-function stringTime() {
-	var t = timeToBeer();
-	var ct = getCurrentTime();
+const stringTime = () => {
 	var dyst = 0;
 	var x = [5, 4, 3, 2, 1, 0, 6];
-	if (ct.getHours() >= 16 && ct.getDay() == 5) {
+	if (currentTime().getHours() >= 16 && currentTime().getDay() == 5) {
 		dyst = 6;
 	} else {
-		dyst = x[ct.getDay()];
+		dyst = x[currentTime().getDay()];
 	}
-	var text = customTimes();
-	changeTitle(`${dyst}:${new Date(t).toISOString().slice(-13, -5)}`)
-	if (text == null) {
+	if (!customTimes()) {
 		document.getElementById('helper').style.display = 'block';
-		return `<span>0${dyst}</span><span>${new Date(t).toISOString().slice(-13, -5).split(':').join('</span><span>')}</span>`;
+		changeTitle(`${dyst}:${new Date(timeToBeer()).toISOString().slice(-13, -5)}`)
+		return `<span>0${dyst}</span><span>${new Date(timeToBeer()).toISOString().slice(-13, -5).split(':').join('</span><span>')}</span>`;
 	} else {
 		document.getElementById('helper').style.display = 'none';
-		return `<span>${text}</span>`;
+		changeTitle(`${customTimes()}`)
+		return `<span>${customTimes()}</span>`;
 	}
 }
 
-function changeTitle(title) {
+const changeTitle = (title) => {
 	document.title = title;
 }
 
-function addHtml(t) {
+const addHtml = (t) => {
 	document.getElementById('timer').innerHTML = t;
 }
 
-(function loopTheShitOutOfIt() {
+(() => {
 	setInterval(() => {
 		addHtml(stringTime());
 	}, 1000);
 })();
 
-function customTimes() {
-	var ct = getCurrentTime();
-	if (ct.getHours() == 16) {
-		if (ct.getMinutes() == 20) {
-			return '420';
-		} else if (ct.getDay() == 5) {
-			return 'Beer O\'Clock';
-		}
+const customTimes = () => {
+	if (currentTime().getHours() == 16 && currentTime().getDay()) {
+		return 'Beer O\'Clock';
 	}
 	return null;
 }
